1d7e36e13e5150ffc374cfc8641026029cc277ee
iamjobin <jobinjobin718@gmail.com>
2026-01-17 20:05:35 +0530

    Add initial implementation of asynchronous agent with calculator functionality

---
+++ agent.py
@@ -0,0 +1,55 @@
+import asyncio
+
+
+async def calculator(expression: str) -> str:
+    await asyncio.sleep(1)
+    return str(eval(expression))
+
+
+async def llm_reasoning(prompt: str) -> dict:
+    if 'add' in prompt:
+        return {
+            "action": "tool",
+            "tool_name": "calculator",
+            "tool_input": "2 + 3"
+        }
+    else:
+        return {
+            "action": "answer",
+            "final_answer": "I can answer directly"
+        }
+    
+
+async def run_agent(user_input: str):
+    memory = []
+
+    print("Agent is started\n")
+
+    while True:
+
+        decision = await llm_reasoning(user_input)
+
+        if decision['action'] == 'answer':
+            print("\nFinal answer: ", decision['final_answer'])
+            break
+
+        if decision['action'] == 'tool':
+            print("\nCalling tool: ", decision['tool_name'])
+            result = await calculator(decision['tool_input'])
+
+            memory.append({
+                "tool": decision['tool_name'],
+                "input": decision['tool_input'],
+                "result": result
+            })
+
+            print("\nFinal Result: ", result)
+            break
+
+
+async def main():
+    user_input = input("Enter your query: ")
+    await run_agent(user_input)
+
+
+asyncio.run(main())
